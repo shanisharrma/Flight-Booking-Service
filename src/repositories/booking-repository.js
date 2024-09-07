cc09e8fb5ec1c321e23ed5bc19edee87c4c3a8a9
codershani <codershani13@gmail.com>
2024-09-07 14:10:26 +0530

    Added Booking Payment Logic

--- src/repositories/booking-repository.js
+++ src/repositories/booking-repository.js
@@ -1,5 +1,7 @@
 const CrudRepository = require("./crud-repository");
 const { Booking } = require("../models");
+const AppError = require("../utils/errors/app-error");
+const { StatusCodes } = require("http-status-codes");
 
 class BookingRepository extends CrudRepository {
   constructor() {
@@ -8,6 +10,25 @@ class BookingRepository extends CrudRepository {
 
   async createBooking(data, transaction) {
     const response = await Booking.create(data, { transaction: transaction });
+    return response;
+  }
+
+  async getWithTransaction(data, transaction) {
+    const response = await this.model.findByPk(data, {
+      transaction: transaction,
+    });
+    if (!response) {
+      throw new AppError("Not able to fund resource.", StatusCodes.NOT_FOUND);
+    }
+    return response;
+  }
+
+  async updateWithTransaction(id, data, transaction) {
+    const response = await this.model.update(
+      data,
+      { where: { id: id } },
+      { transaction: transaction }
+    );
     return response;
   }
 }
--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -4,6 +4,8 @@ const { ServerConfig } = require("../config");
 const { StatusCodes } = require("http-status-codes");
 const AppError = require("../utils/errors/app-error");
 const { BookingRepository } = require("../repositories/booking-repository");
+const { Enums } = require("../utils/common");
+const { BOOKED, CANCELED, INITIATED, PENDING } = Enums.BOOKING_STATUS;
 
 const bookingRepository = new BookingRepository();
 
@@ -42,6 +44,52 @@ async function createBooking(data) {
   }
 }
 
+async function makePayment(data) {
+  const transaction = await db.sequelize.transaction();
+  try {
+    const bookingDetails = await bookingRepository.getWithTransaction(
+      data.bookingId,
+      transaction
+    );
+    if (bookingDetails.status == CANCELED) {
+      throw new AppError("The booking has Expired.", StatusCodes.BAD_REQUEST);
+    }
+    const bookingTime = new Date(bookingDetails.createdAt);
+    const currentTime = new Date();
+    if (currentTime - bookingTime > 300000) {
+      await bookingRepository.update(
+        data.bookingId,
+        { status: CANCELED },
+        transaction
+      );
+      throw new AppError("The booking has expired", StatusCodes.BAD_REQUEST);
+    }
+    if (bookingDetails.totalCost != data.totalCost) {
+      throw new AppError(
+        "The amount of the payment doesn't match",
+        StatusCodes.BAD_REQUEST
+      );
+    }
+    if (bookingDetails.userId != data.userId) {
+      throw new AppError(
+        "The user corresponding to the booking doesn't match",
+        StatusCodes.BAD_REQUEST
+      );
+    }
+    // we assume here that payment is successful
+    await bookingRepository.update(
+      data.bookingId,
+      { status: BOOKED },
+      transaction
+    );
+    await transaction.commit();
+  } catch (error) {
+    await transaction.rollback();
+    throw error;
+  }
+}
+
 module.exports = {
   createBooking,
+  makePayment,
 };
